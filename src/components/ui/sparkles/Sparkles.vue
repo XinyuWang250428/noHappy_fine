5bf0184f89104c6a7097d1f707944165b5b23a72
XinyuWang250428 <wxy250428@163.com>
2025-06-30 02:40:54 +0800

    🔧🔧🔧

--- src/components/ui/sparkles/Sparkles.vue
+++ src/components/ui/sparkles/Sparkles.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// @ts-nocheck
 import { ref, onMounted, computed } from 'vue';
 import { loadSlim } from "@tsparticles/slim";
 import { tsParticles } from "@tsparticles/engine";
